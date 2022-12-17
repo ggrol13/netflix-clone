f9fc7916bb6ee892480ebf7adb78409af4fc3d25
ggrol13 <ggrol13@gmail.com>
2022-12-17 10:10:36 +0900

    build entities 2

---
+++ src/apis/user/entities/account.entity.ts
@@ -0,0 +1,44 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ProfileEntity } from './profile.entity';
+
+@Entity('account')
+export class AccountEntity extends BaseEntity {
+  //PK가 될 수 있는 조건 2 개
+  //1. auto increment
+  //2. uuid
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 128, name: 'password', nullable: false })
+  password: string;
+
+  @Column({ length: 15, name: 'email', nullable: false })
+  email: string;
+
+  @Column({ length: 1, name: 'gender', nullable: false })
+  gender: string;
+
+  @Column({ length: 15, name: 'phone_num', nullable: false })
+  phoneNum: string;
+
+  @Column({ type: Date, name: 'birth_date' })
+  birthDate: Date;
+
+  @CreateDateColumn({ name: 'created_at', type: Date })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: Date })
+  updatedAt: Date;
+
+  @OneToMany(() => ProfileEntity, (profile) => profile.account)
+  profile: ProfileEntity[];
+}
--- src/apis/user/entities/profile.entity.ts
+++ src/apis/user/entities/profile.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { AccountEntity } from './account.entity';
+
+@Entity('profile')
+export class ProfileEntity extends BaseEntity {
+  //PK가 될 수 있는 조건 2 개
+  //1. auto increment
+  //2. uuid
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 10, name: 'name', nullable: false })
+  name: string;
+
+  @Column({ length: 256, name: 'thumbnail', nullable: false })
+  thumbnail: string;
+
+  @Column({ length: 15, name: 'language', nullable: false })
+  language: string;
+
+  @Column({ type: 'integer', name: 'age_limit', nullable: false })
+  ageLimit: number;
+
+  @CreateDateColumn({ name: 'created_at', type: Date })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: Date })
+  updatedAt: Date;
+
+  @ManyToOne(() => AccountEntity, (account) => account.id)
+  @JoinColumn({ name: 'account_id' })
+  account: AccountEntity;
+}
